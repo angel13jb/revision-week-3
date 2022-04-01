27f7ae01db9c22480fad6c39f74dd28c5b25e92a
angel13jb@gmial.com <angel13jb@gmail.com>
2022-04-01 10:16:25 +0100

    revision week 3

---
+++ src/Programme_1_OddEvenNumber.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Programme_1_OddEvenNumber {
+
+//program for odd even ternary operator
+    public static void main(String[] args) {
+        //Scanner declaration for reading input form console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number:");
+        int number = scanner.nextInt();
+        isItOddOrEvenNumber(number);
+        // closing the scanner object
+        scanner.close();
+    }
+
+    // Checking the number is even or odd
+    public static void isItOddOrEvenNumber(int number) {
+        //ternary operator is used
+        String evenOrOdd = (number % 2 == 0) ? "Even" : "Odd";
+        System.out.println("The " + number + " is " + evenOrOdd + " number");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
